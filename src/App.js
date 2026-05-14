bd3124a299f87a027065804195b5100b569a97ff
Ana Ruas <anaruas.professional@outlook.com>
2026-05-14 11:27:06 -0300

    feat: add write and settings page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
+import Settings from "./components/pages/settings/Settings";
 import Single from "./components/pages/single/Single";
 import TopBar from "./components/topBar/TopBar";
+import Write from "./components/write/Write";
 
 function App() {
   return (
     <>
       <TopBar />
-      <Single />
+      <Settings />
     </>
   );
 }
